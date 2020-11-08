50dd8ff9909c52ab2ea08719d72cd46f9c4d395e
方志钢 <deanfang@fangzhigangdeMacBook-Pro.local>
2020-11-08 18:17:57 +0800

    update

--- common/utils.py
+++ common/utils.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 
+import datetime
+
 
 def format_args_get(args):
     format_args = dict()
@@ -7,3 +9,12 @@ def format_args_get(args):
         format_args[k] = v[0].decode(encoding='utf-8')
     return format_args
 
+
+def tracer(func):
+    def wrapper(**kwargs):
+        start_time = datetime.datetime.now()
+        res = func(**kwargs)
+        end_time = datetime.datetime.now()
+        print((end_time - start_time).seconds)
+        return res
+    return wrapper
--- modules/employee/controller.py
+++ modules/employee/controller.py
@@ -1,21 +1,26 @@
 # -*- coding:utf-8 -*-
 import modules.employee.service as employee_service
+import common.utils
 import logging
 
 
+@common.utils.tracer
 def get_employees(**kwargs):
     logging.info(kwargs)
     return employee_service.get_employees(kwargs)
 
 
+@common.utils.tracer
 def add_employees(**kwargs):
     return employee_service.add_employees(kwargs)
 
 
+@common.utils.tracer
 def update_employees(**kwargs):
     return employee_service.update_employees(kwargs)
 
 
+@common.utils.tracer
 def delete_employees(**kwargs):
     return employee_service.delete_employees(kwargs)
 
@@ -30,4 +35,4 @@ def dispatcher(self, action, **kwargs):
     if action == "delete":
         return delete_employees(**kwargs)
     if action == "healthCheck":
-        return {"healthcheck": "OK"}
+        return {"health": "OK"}
